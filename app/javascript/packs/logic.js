6d689662e3942eaf0be5eacf22c1a4bfdbb2b566
Matthew Zenn <spyguy57@gmail.com>
2021-08-04 15:45:04 -0400

    rerouted help link

--- app/javascript/packs/logic.js
+++ app/javascript/packs/logic.js
@@ -2,27 +2,19 @@ var maximize = document.getElementById("maximize");
 var minimize = document.getElementById("minimize");
 var google = document.getElementById("google");
 var about = document.getElementById("about");
-var panel = document.getElementById("panel");
-var logo = document.querySelector("logo");
-var title = document.querySelector("title");
 
 function submitForm() {
     shell.openExternal('https://www.google.com/search?q='+document.getElementById('search').value);
 }
 
-function help() {
-    shell.openExternal('https://github.com/MatthewZenn/Matter');
-}
-
 function Move() {
-    if (panel.style.left == '-20%') {
-        panel.style.left = '0';
+    if (document.getElementById('panel').style.left == '-20%') {
+        document.getElementById('panel').style.left = '0';
     }
 
     else {
-        panel.style.left = '-20%';
+        document.getElementById('panel').style.left = '-20%';
     }
 }
 
-logo.addEventListener("click", Move);
-title.addEventListener("click", Move);
+window.Move = Move;
